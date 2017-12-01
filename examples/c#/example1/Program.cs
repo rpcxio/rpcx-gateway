dd7d3bff1e51f930f1c02f72cacd4f02a0922bad
smallnest <smallnest@gmail.com>
2017-12-01 17:06:48 +0800

    add c# client example

---
+++ examples/c#/example1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;  
+using System.IO;  
+using System.Net;  
+using System.Text;  
+
+namespace example1
+{
+    class Program
+    {
+        public static void Main ()  
+        {  
+            // write request
+            WebRequest request = WebRequest.Create ("http://vps.colobu.com:9981/"); 
+            request.Method = "POST";  
+            request.Headers.Add("X-RPCX-MessageID", "12345678");
+            request.Headers.Add("X-RPCX-MesssageType", "0");
+            request.Headers.Add("X-RPCX-SerializeType", "1");
+            request.Headers.Add("X-RPCX-ServicePath", "Arith");
+            request.Headers.Add("X-RPCX-ServiceMethod", "Mul");
+
+            string postData = "{\"A\":10, \"B\":20}";  
+            byte[] byteArray = Encoding.UTF8.GetBytes (postData);  
+            request.ContentType = "application/rpcx";  
+            request.ContentLength = byteArray.Length;  
+            Stream dataStream = request.GetRequestStream ();  
+            dataStream.Write (byteArray, 0, byteArray.Length);    
+            dataStream.Close ();  
+
+            
+            // Get the response
+            WebResponse response = request.GetResponse ();   
+            dataStream = response.GetResponseStream ();  
+            StreamReader reader = new StreamReader (dataStream);  
+            string responseFromServer = reader.ReadToEnd ();  
+
+            Console.WriteLine(responseFromServer);  
+
+            // Clean up the streams
+            reader.Close ();  
+            dataStream.Close ();  
+            response.Close ();  
+        } 
+    }
+}
